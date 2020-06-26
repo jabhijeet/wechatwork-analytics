ab572ed3791cf32c848ae6380d65aa99381ffe03
Abhijeet <jabhijeet@gmail.com>
2020-06-26 15:50:04 +0800

    Charts integration with services

--- frontend/src/components/AnalyticsCard.js
+++ frontend/src/components/AnalyticsCard.js
@@ -18,6 +18,7 @@ class AnalyticsCard extends Component {
         super(props);
         let number = Math.floor((Math.random() * colors.length));
         this.state = {
+            id: props.id,
             color: colors[number],
             header: props.header,
             title: props.title,
@@ -25,14 +26,34 @@ class AnalyticsCard extends Component {
             text: 'Loading...',
             loading: true
         };
+        this.getOutput = this.getOutput.bind(this);
     }
 
     async componentDidMount() {
         let response = await fetch(this.state.api);
         let body = await response.json();
         console.log(body);
-        this.state = {text: body.name, loading: false};
-        this.setState(this.state);
+        var state = {
+            text: this.getOutput(body),
+            loading: false
+        };
+        this.setState(state);
+    }
+
+    getOutput(body) {
+        let id = this.state.id;
+        if (id === "TotalDepartments") {
+            return body.department.length;
+        } else if (id === "TotalCompanyClients") {
+            var totalClients = 0;
+            for (const [key, value] of Object.entries(body)) {
+                totalClients = totalClients + value;
+            }
+            return totalClients;
+        } else if (id === "TotalCompanyEmployees") {
+            return body.follow_user.length;
+        }
+        return body;
     }
 
     render() {
@@ -40,7 +61,8 @@ class AnalyticsCard extends Component {
             <Card body style={{backgroundColor: this.state.color}}>
                 <CardHeader style={{backgroundColor: '#0192'}}>{this.state.header}</CardHeader>
                 <CardBody>
-                    {this.state.loading ? 'Loading...' : <CardText>{this.state.text}</CardText>}
+                    {this.state.loading ? 'Loading...' :
+                        <CardText style={{color: "#89827", fontSize: 45}}>{this.state.text}</CardText>}
                 </CardBody>
             </Card>
         );
